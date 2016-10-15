673d1ea9baf19c152c9d72272ab8eb89ef232e60
Kelly Campbell <Kcampbell16@cnm.edu>
2016-10-15 12:34:52 -0600

    pic section

--- index.php
+++ index.php
@@ -16,6 +16,8 @@
 		<section>
 			Simply put passion and love is the motivation behind everything I do. Im supported by some pretty awesome humans and I cherish them all. Word can never tell a story like a picture can. So as you have visited I hope you simply sit back and enjoy some happy moments with the people that matter most in my life.
 		</section>
+		<h1> This is what life is all about</h1>
+
 
 
 
