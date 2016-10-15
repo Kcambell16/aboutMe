dc26411ef6430ca97b98bac6687e24352dad9097
Kelly Campbell <Kcampbell16@cnm.edu>
2016-10-15 12:33:35 -0600

    text

--- index.php
+++ index.php
@@ -7,11 +7,16 @@
 		<link href="css/stylesheet.css" rel="stylesheet" type="text/css"/>
 		<title>Kelly Danielle Campbell </title>
 	</head>
+
 	<h1> Kelly Danielle Campbell</h1>
 	<body>
 		<!--below pictures of all the people and things I love--->
 		<img src="img/kcampbell1.jpg" alt="kcampbell">
-		
+			<h2> About Me!</h2>
+		<section>
+			Simply put passion and love is the motivation behind everything I do. Im supported by some pretty awesome humans and I cherish them all. Word can never tell a story like a picture can. So as you have visited I hope you simply sit back and enjoy some happy moments with the people that matter most in my life.
+		</section>
+
 
 
 
