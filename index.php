6b0e93529e524a3948bbebb8d27c492793bf8a50
Kelly Campbell <Kcampbell16@cnm.edu>
2016-10-15 12:09:33 -0600

    adding photo files

--- index.php
+++ index.php
@@ -5,6 +5,15 @@
 
 		<!-- Add css file link-->
 		<link href="css/stylesheet.css" rel="stylesheet" type="text/css"/>
-
+		<title>Kelly Danielle Campbell </title>
 	</head>
+	<h1> Kelly Danielle Campbell</h1>
+	<body>
+		<!--below pictures of all the people and things I love--->
+		<img src="img/kcampbell.jpg" alt="kcampbell">
+
+
+
+
+	</body>
 </html>
