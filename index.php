049323404691f1fd78c804f413b404b01389c62f
Kelly Campbell <Kcampbell16@cnm.edu>
2016-10-15 10:00:57 -0600

    start of aboutme

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE hmtl>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+
+		<!-- Add css file link-->
+		<link href="css/stylesheet.css" rel="stylesheet" type="text/css"/>
+
+	</head>
+</html>
