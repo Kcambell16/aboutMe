7f348f7c1ae623af01c0217e34e3fff846065280
Kelly Campbell <Kcampbell16@cnm.edu>
2016-10-17 09:50:00 -0600

    images

--- index.php
+++ index.php
@@ -17,6 +17,13 @@
 			Simply put passion and love is the motivation behind everything I do. Im supported by some pretty awesome humans and I cherish them all. Word can never tell a story like a picture can. So as you have visited I hope you simply sit back and enjoy some happy moments with the people that matter most in my life.
 		</section>
 		<h1> This is what life is all about</h1>
+		People I love
+			<img src="img/family1.jpg" alt=family1">
+		<img src="img/family2.jpg" alt=family2">
+		<img src="img/family3.jpg" alt="family3">
+		<img src="img/IMG_0039.PNG" alt="IMG39">
+		<img src="img/IMG_1840.JPG" alt="IMG1840">
+
 
 
 
